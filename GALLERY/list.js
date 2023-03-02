c2cce3baf919a865bfd156e22fa59c2ec38b9efc
petarsladojevic <114730022+petarsladojevic@users.noreply.github.com>
2023-03-02 10:06:43 +0100

    Add files via upload

--- GALLERY/list.js
+++ GALLERY/list.js
@@ -1,12 +1,14 @@
-"paper_Mon_Dec_19_2022_21_09_21.png",
-"paper_Sat_Dec_10_2022_15_24_09.png",
-"paper_Sat_Dec_17_2022_22_25_24.png",
-"paper_Sat_Dec_17_2022_22_25_44.png",
-"paper_Sat_Jan_21_2023_08_55_06.png",
-"paper_Sun_Dec_18_2022_17_13_20.png",
-"paper_Sun_Jan_22_2023_10_22_39.png",
-"paper_Sun_Jan_22_2023_21_46_47.png",
-"paper_Thu_Dec_08_2022_21_49_06.png",
-"paper_Thu_Dec_15_2022_21_33_25.png",
-"paper_Thu_Dec_15_2022_22_03_31.png",
-"paper_Wed_Dec_14_2022_10_13_16.png"
+{
+"0": "paper_Mon_Dec_19_2022_21_09_21.png",
+"1": "paper_Sat_Dec_10_2022_15_24_09.png",
+"2": "paper_Sat_Dec_17_2022_22_25_24.png",
+"3": "paper_Sat_Dec_17_2022_22_25_44.png",
+"4": "paper_Sat_Jan_21_2023_08_55_06.png",
+"5": "paper_Sun_Dec_18_2022_17_13_20.png",
+"6": "paper_Sun_Jan_22_2023_10_22_39.png",
+"7": "paper_Sun_Jan_22_2023_21_46_47.png",
+"8": "paper_Thu_Dec_08_2022_21_49_06.png",
+"9": "paper_Thu_Dec_15_2022_21_33_25.png",
+"10": "paper_Thu_Dec_15_2022_22_03_31.png",
+"11": "paper_Wed_Dec_14_2022_10_13_16.png"
+}
