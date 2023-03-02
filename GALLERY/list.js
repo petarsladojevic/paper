63eb3aac4f1be6f0da249954b9aab54c5a849f94
petarsladojevic <114730022+petarsladojevic@users.noreply.github.com>
2023-03-02 09:21:01 +0100

    Add files via upload

---
+++ GALLERY/list.js
@@ -0,0 +1,12 @@
+"paper_Mon_Dec_19_2022_21_09_21.png",
+"paper_Sat_Dec_10_2022_15_24_09.png",
+"paper_Sat_Dec_17_2022_22_25_24.png",
+"paper_Sat_Dec_17_2022_22_25_44.png",
+"paper_Sat_Jan_21_2023_08_55_06.png",
+"paper_Sun_Dec_18_2022_17_13_20.png",
+"paper_Sun_Jan_22_2023_10_22_39.png",
+"paper_Sun_Jan_22_2023_21_46_47.png",
+"paper_Thu_Dec_08_2022_21_49_06.png",
+"paper_Thu_Dec_15_2022_21_33_25.png",
+"paper_Thu_Dec_15_2022_22_03_31.png",
+"paper_Wed_Dec_14_2022_10_13_16.png"
